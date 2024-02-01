a9ad5f2b5037d55cbdebd8f2972684ae4e0579e5
Phil Varner <pvarner@element84.com>
2024-02-01 18:15:05 -0500

    remove 3.12, more work needs to be done for that version

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
     license="Apache-2.0",
     include_package_data=True,
