b01d4817144cf9018c432a2effd13cc0cc7575ba
Phil Varner <pvarner@element84.com>
2024-02-01 18:21:36 -0500

    update dependencies, as sphinx 4.5 doesn't work on apple silicon (#6)
    * update dependencies, as sphinx 4.5 doesn't work on apple silicon

--- setup.py
+++ setup.py
@@ -7,46 +7,47 @@ from setuptools import setup, find_namespace_packages
 from src.cirrus.plugins.docs.constants import DESC
 
 
-NAME = 'cirrus-docs'
+NAME = "cirrus-docs"
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = os.environ.get('PLUGIN_VERSION', '0.0.0')
+VERSION = os.environ.get("PLUGIN_VERSION", "0.0.0")
 
 
-with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
     readme = f.read()
 
-with open(os.path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
-    reqs = f.read().split('\n')
+with open(os.path.join(HERE, "requirements.txt"), encoding="utf-8") as f:
+    reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in reqs if 'git+' not in x]
+install_requires = [x.strip() for x in reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in reqs if "git+" not in x]
 
 
 setup(
     name=NAME,
-    packages=find_namespace_packages('src'),
-    package_dir={'': 'src'},
+    python_requires=">=3.9",
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
     version=VERSION,
     description=DESC,
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Jarrett Keifer (jkeifer), Element 84',
-    url='https://github.com/cirrus-geo/cirrus-docs',
+    long_description_content_type="text/markdown",
+    author="Jarrett Keifer (jkeifer), Element 84",
+    url="https://github.com/cirrus-geo/cirrus-docs",
     install_requires=install_requires,
     dependency_links=dependency_links,
-        classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
-    license='Apache-2.0',
+    license="Apache-2.0",
     include_package_data=True,
-    entry_points=f'''
+    entry_points=f"""
         [cirrus.plugins]
         {NAME}=cirrus.plugins.docs
         [cirrus.commands]
         docs=cirrus.plugins.docs.commands:docs
-    ''',
+    """,
 )
